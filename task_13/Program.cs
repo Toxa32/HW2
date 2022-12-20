c9cf07d22814af43ca49c856d8ff7ff7092d840d
anton <gumen.tosha@yandex.ru>
2022-12-20 18:48:41 +0300

    DZ-2

---
+++ task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(num);
+
+if (num > 3)
+{
+    Console.WriteLine(str[2]);
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
